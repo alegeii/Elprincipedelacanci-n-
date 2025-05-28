821df47cbc6a207d30de895a8d820f2997591ce9
alegeii <vpya2225i@gmail.com>
2025-05-28 10:43:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+windows.onload = function() {
+var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "")
+alert ( "Es un gusto tenerte aquí " + reply + "!")
+} 
+let miBoton = document.querySelector('button');
+miBoton.onclick = funtion() { 
+       aler('Con el código: JT05 obtendrás un 15% en tu próxima compra');
+} 
